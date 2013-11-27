b992f51ee20cca3bbc75ee29fdbfc260ed51d47c
Eduardo Calambro <eduardo.calambrojr@mobext.com>
2013-11-27 22:00:12 +0800

    git push -u origin master

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,4 +4,32 @@
   	<input type="password" id="password" name="password" placeholder="Password" />
   	<input type="submit" id="login" name="login" value="Login" />
   </div><!-- end content -->
+    <div data-role="popup" data-history="false" data-dismissible="false" data-overlay-theme="d" id="alert" data-history="false" data-transition="pop" data-position-to="window">
+      <div data-role="content" align="center">
+            <a href="#" id="closePopup" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext">Close</a>
+          <p id="alertDesc"></p>
+      </div>
+    </div>
 </div><!-- end page -->
+<script type="text/javascript">
+
+$('#document').ready(function(){
+
+	$('#login').click(function(){
+
+		var email = $('#email').val();
+		var password = $('#password').val();
+		var response = {};
+
+		response = login(email,password);
+
+
+
+	});
+
+
+
+});
+
+
+</script>
