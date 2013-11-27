e9506dd70a90b7c5c41cf0b811e3885448110816
joseph <josephbryan02@gmail.com>
2013-11-27 22:01:48 +0800

    git push -u origin master

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,12 +5,15 @@ class Home extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+
+		$this->load->model('user_model');
 	}//end construct()
 
 	public function index()
 	{
 
 		$data['page'] = 'home_view';
+		$this->user_model->insertRecord('12', '23', '23', 'adsadsadasd', '1');
 		$this->load->view('page_holder',$data);
 
 	}//end index()
