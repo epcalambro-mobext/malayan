d47400187844b6989cbfff4887ce846a5bf6dcc3
joseph <josephbryan02@gmail.com>
2013-11-27 22:03:20 +0800

    git push -u origin master

---
+++ application/models/user_model.php
@@ -0,0 +1,33 @@
+<?php
+	if(! DEFINED('BASEPATH')) exit("No direct access allowed");
+
+	class User_Model extends CI_Model
+	{
+		public function __construct()
+		{
+			parent::__construct();
+
+			$this->load->database();
+		}
+
+		public function insertRecord($gender, $weight, $height, $mHistory, $hId)
+		{
+			$data = array(
+					'U_Gender' => $gender,
+					'U_Weight' => $weight,
+					'U_Height' => $height,
+					'U_Medical_History' => $mHistory,
+					'H_Id' => $hId 
+					);
+
+			if($this->db->insert('tbl_users', $data))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+?>
